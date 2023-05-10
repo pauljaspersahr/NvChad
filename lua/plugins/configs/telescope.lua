189d231fae0e0e4128feead60cc92d3843ef14a1
pjs <pauljasper.sahr@gmail.com>
2023-05-10 09:58:10 +0200

    added keybindings for telescope

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -46,6 +46,10 @@ local options = {
     buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
     mappings = {
       n = { ["q"] = require("telescope.actions").close },
+      i = {
+        ["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+        ["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+        },
     },
   },
 
